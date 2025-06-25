07f90a6a8c13cc5aeb5dab7a7c087731c6ef9a6d
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-25 16:44:27 +0200

    Aggiungi la funzione hobbyList per restituire un array di hobby e aggiorna l'esportazione

--- hobbies.js
+++ hobbies.js
@@ -3,3 +3,13 @@ hobbies.js
 Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 Esporta la funzione dal file.
 */
+
+function hobbyList(hobbyOne, HobbyTwo, hobbyThree) {
+	const hobbyList = [hobbyOne, HobbyTwo, hobbyThree]
+	return hobbyList
+}
+
+console.log(hobbyList("hobby one", "hobby two", "hobby three"))
+
+
+module.exports = hobbyList
