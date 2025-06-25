d2789beb00a014340b4706d8deea40353c1ca54d
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-25 15:48:56 +0200

    Correggi la funzione nameList per restituire un oggetto con firstName e lastName

--- name.js
+++ name.js
@@ -1,3 +1,16 @@
 /* - names.js
 Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 Esporta la funzione dal file.*/
+
+function nameList(firtsName, lastName) {
+	const nameFirstLast = {
+		firtsName: firtsName,
+		lastName: lastName
+	}
+	return nameFirstLast
+}
+console.log(nameFirstLast)
+
+export default nameFirstLast
+
+//export default per esportare un'unica entità principale da un modulo. //export esporta più elementi da un modulo
